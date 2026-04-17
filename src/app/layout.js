b16ad9674975e92bfafdfe59a2aa74a1384acd8d
Siam-AR <siam.ar.nexus@gmail.com>
2026-04-17 19:39:55 +0600

    implement dynamic homePage & react-toastify

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Foooter";
+import Footer from "@/components/Footer";
 import { DataProvider } from "@/app/context/DataContext";
-
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +30,7 @@ export default function RootLayout({ children }) {
         <DataProvider>
         <Navbar></Navbar>
         {children}
+        <ToastContainer />
         <Footer></Footer>
         </DataProvider>
         </body>
